1cf66d090fae5cb72c0ba341d935d59052947789
Sandeepa-git <agsvwimalasiri@gmail.com>
2025-11-18 17:44:29 +0530

    Remove 'Get started' button and update navbar for hackathon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,29 +3,26 @@ import { base, heading } from "@/constants/fonts";
 import { cn } from "@/lib";
 import "@/styles/globals.css";
 import { generateMetadata } from "@/utils";
-import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = generateMetadata();
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={cn(
-                    "min-h-screen bg-[#101010] text-foreground font-base antialiased overflow-x-hidden dark",
-                    base.variable,
-                    heading.variable,
-                )}
-            >
-                <ClerkProvider>
-                    <Toaster richColors theme="dark" position="bottom-center" />
-                    {children}
-                </ClerkProvider>
-            </body>
-        </html>
-    );
-};
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-[#101010] text-foreground font-base antialiased overflow-x-hidden dark",
+          base.variable,
+          heading.variable
+        )}
+      >
+        <Toaster richColors theme="dark" position="bottom-center" />
+        {children}
+      </body>
+    </html>
+  );
+}
